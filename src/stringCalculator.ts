a342a51883b210513d50f48ba64a9faefe8cc91d
Priyanka Sankhala <priyanka.sankhala@HW1445.local>
2025-04-14 16:51:08 +0530

    setup project

---
+++ src/stringCalculator.ts
@@ -0,0 +1,12 @@
+const add = (input:string): number =>{
+    if (!input) return 0;
+    return 1
+
+}
+
+const extractDelimiters = (input: string) : [] =>{
+    //findout the delimiters and provide the array of numbers 
+    const delimiters = [',', '\n',':', ';'];
+    console.log(input);
+    return [];
+}
